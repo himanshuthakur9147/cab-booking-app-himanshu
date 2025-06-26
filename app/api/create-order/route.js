29fb480fa21cab49c6c4de96ff044d50b8462360
abhay chittod <growmore056@gmail.com>
2025-06-26 16:47:56 +0530

    api key rename

--- app/api/create-order/route.js
+++ app/api/create-order/route.js
@@ -2,8 +2,8 @@ import Razorpay from "razorpay";
 import { NextResponse } from "next/server";
 
 const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET,
+  key_id: process.env.REAL_KEY_ID,
+  key_secret: process.env.REAL_KEY_SECRET,
 });
 
 export async function POST(req) {
