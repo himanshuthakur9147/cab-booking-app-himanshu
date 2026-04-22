4cf31924aca03ecdc86a3bcb59ceb8bc6f55e00f
Vaishnavi Sharma <158552361+vaishnavi2011sharma@users.noreply.github.com>
2026-04-22 21:01:19 +0530

    Create layout.jsx Rameshwaram SEO 21-04-2026

---
+++ app/tempo-traveller-in-rameshwaram/layout.jsx
@@ -0,0 +1,24 @@
+// app/tempo-traveller-in-rameshwaram/layout.jsx
+
+export const metadata = {
+  title: "Book  AC Tempo Traveller in Rameshwaram  15% Off on Group Bookings | Yatra Travel India",
+  description:
+    "Planning a Rameshwaram temple trip? Hire a comfortable tempo traveller with driver. Call Yatra Travel India at 91 9044019511 now.",
+  keywords:
+    "Keywords: Tempo Traveller for Temple Tour in Rameswaram, Rameswaram Group Travel Tempo Traveller, Tempo Traveller on Hire in Rameswaram,12 Seater Tempo Traveller in Rameswaram, 17 Seater Tempo Traveller in Rameswaram, Luxury Tempo Traveller in Rameswaram, Tempo Traveller Rate in Rameswaram, Tempo Traveller Charge in Rameswaram, Tempo Traveller per Km in Rameswaram, Outstation Group Vehicle in Rameswaram, Rameswaram Sightseeing Tempo Traveller, Dhanushkodi Tour Tempo Traveller,",
+  openGraph: {
+    title: "Book  AC Tempo Traveller in Rameshwaram  15% Off on Group Bookings | Yatra Travel India",
+    description:
+      "Planning a Rameshwaram temple trip? Hire a comfortable tempo traveller with driver. Call Yatra Travel India at 91 9044019511 now.",
+    url: "https://www.yatratravelindia.com/tempo-traveller-in-rameshwaram ",
+    siteName: "Yatra Travel India",
+    type: "website",
+  },
+  alternates: {
+    canonical: "https://www.yatratravelindia.com/tempo-traveller-in-rameshwaram ",
+  },
+};
+
+export default function RameshwaramLayout({ children }) {
+  return <>{children}</>;
+}
