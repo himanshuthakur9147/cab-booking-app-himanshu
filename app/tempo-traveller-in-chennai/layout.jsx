1e3bf7be205b9066c72d4b88d1b28eab7e915a4b
Vaishnavi Sharma <158552361+vaishnavi2011sharma@users.noreply.github.com>
2026-04-22 20:38:41 +0530

    Create layout.jsx Chennai SEO Final 21-04-2026

---
+++ app/tempo-traveller-in-chennai/layout.jsx
@@ -0,0 +1,24 @@
+// app/tempo-traveller-in-chennai/layout.jsx
+
+export const metadata = {
+  title: "Rent Tempo Traveller in Chennai 10%OFF  Yatra Travel India",
+  description:
+    "Book tempo traveller in Chennai for family, office & tour travel. Spacious, AC vehicles with fast booking. Call +91 9044019511 | Yatra Travel India",
+  keywords:
+    " Keywords-Tempo Traveller in Chennai, Tempo Traveller on Rent in Chennai, Tempo Traveller Hire in Chennai, Tempo Traveller Booking in Chennai, Tempo Traveller per Km Rate in Chennai, Tempo Traveller Price in Chennai, Tempo Traveller Fare in Chennai, Tempo Traveller Cost in Chennai, Tempo Traveller with Driver in Chennai, Outstation Tempo Traveller in Chennai, Group Travel in Chennai",
+  openGraph: {
+    title: "Rent Tempo Traveller in Chennai 10%OFF  Yatra Travel India",
+    description:
+      "Book tempo traveller in Chennai for family, office & tour travel. Spacious, AC vehicles with fast booking. Call +91 9044019511 | Yatra Travel India",
+    url: "https://www.yatratravelindia.com/tempo-traveller-in-chennai ",
+    siteName: "Yatra Travel India",
+    type: "website",
+  },
+  alternates: {
+    canonical: "https://www.yatratravelindia.com/tempo-traveller-in-chennai ",
+  },
+};
+
+export default function ChennaiLayout({ children }) {
+  return <>{children}</>;
+}
